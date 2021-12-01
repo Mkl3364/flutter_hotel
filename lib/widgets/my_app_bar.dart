72c8eac57e5221043345082621b53196401a0d2e
Mickael Brunet <mickael.brunet64@gmail.com>
2021-12-01 22:25:41 +0100

    Add a custom appbar

---
+++ lib/widgets/my_app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => const Size.fromHeight(50);
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('Explore', style: GoogleFonts.nunito(
+        color: Colors.black,
+        fontSize: 22,
+        fontWeight: FontWeight.w800
+      )),
+      backgroundColor: Colors.white,
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+          color: Colors.grey[800],
+          size: 20
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.favorite_outline_rounded,
+          color: Colors.grey[800],
+          size: 20,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.place,
+          color: Colors.grey[800],
+          size: 20,
+          ),
+          onPressed: () {},
+        )
+      ],
+      centerTitle: true,
+    );
+  }
+}
