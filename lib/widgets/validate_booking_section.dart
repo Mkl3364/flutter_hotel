6fd67cb07c509146fe82d4badc35a4b4c391a218
Mickael Brunet <mickael.brunet64@gmail.com>
2021-12-02 23:17:11 +0100

    Add three new widget to hold the calendar page

---
+++ lib/widgets/validate_booking_section.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ValidateBookingSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
