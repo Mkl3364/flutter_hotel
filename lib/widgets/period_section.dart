19656b8620d1e5c51d3bdd90f75990d5feb1b392
Mickael Brunet <mickael.brunet64@gmail.com>
2021-12-03 13:25:54 +0100

    Add the period_section widget

--- lib/widgets/period_section.dart
+++ lib/widgets/period_section.dart
@@ -3,6 +3,72 @@ import 'package:flutter/material.dart';
 class PeriodSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Départ',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                const Text(
+                  'Month 12 Dec',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 60,
+              width:1,
+              color: Colors.grey[350]
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Return',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                const Text(
+                  'Tue 22 Dec',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  )
+                )
+              ],
+            )
+          ],
+        ),
+        const Divider(
+          color: Colors.grey,
+          height: 1,
+        ),
+      ],
+    );
   }
 }
