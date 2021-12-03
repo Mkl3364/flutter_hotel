dd53293b2905d71eb134b9c46196094b8c7f0fcb
Mickael Brunet <mickael.brunet64@gmail.com>
2021-12-03 14:35:05 +0100

    Implement the validation of a booking section

--- lib/widgets/validate_booking_section.dart
+++ lib/widgets/validate_booking_section.dart
@@ -1,8 +1,52 @@
 import 'package:flutter/material.dart';
 
+const d_green = Color(0xFF54D3C2);
+
 class ValidateBookingSection extends StatelessWidget {
+
+  final selectedRadio = 1;
+
   @override
+
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        RadioListTile(
+          value: selectedRadio,
+          activeColor: d_green,
+          groupValue: selectedRadio,
+          selected: true,
+          title: const Text(
+            "Flexible dates",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20.0,
+            ),
+          ),
+          onChanged: null,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          width: double.infinity,
+          child: ElevatedButton(
+            child: const Text(
+              'Apply',
+              style: TextStyle(fontSize: 17),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: d_green,
+              padding: const EdgeInsets.all(15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+            ),
+            onPressed: () {
+              const Text('Apply booking');
+              Navigator.pop(context);
+            },
+          )
+        )
+      ],
+    );
   }
 }
